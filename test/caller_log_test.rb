38271d21ae581e301491d8215397813ffbb2a322
ken <block24block@gmail.com>
2017-03-31 15:54:17 +0800

    look nicer

--- test/caller_log_test.rb
+++ test/caller_log_test.rb
@@ -7,23 +7,18 @@ class CallerLogTest < Minitest::Test
       action n - 1 unless n == 0
     end
 
-    def run n
-      run n - 1 unless n == 0
-    end
-
     def self.new_and_go n
       k = K.new
-      k.run n
       k.action n
     end
   end
 
   def test_it_does_something_useful
     CallerLog.log K, '/tmp/caller_log_test.html'
-    ts = (1..3).map do |n|
+    ts = (1..4).map do |n|
       Thread.new do
         n.times do
-          K.new_and_go n
+          K.new_and_go n * 20
         end
       end
     end
